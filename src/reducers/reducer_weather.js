77d62c79b29913b8808caabd9bad8d7ed5f382ff
LeoSL <leosilvalima@gmail.com>
2017-05-07 18:23:50 -0400

    finishing weatherchannel with google maps topping

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,8 +3,8 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      // identical to below _ return state.concat([action.payload.data]); _
-      return [ action.payload.data, ...state ]; // < flatten 
+      // identical to below: return state.concat([action.payload.data]); _
+      return [ action.payload.data, ...state ]; // < flatten way
   }
   return state;
 }
